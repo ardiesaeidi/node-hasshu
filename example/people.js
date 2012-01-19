0167595001fa8b064c3a37193b311acdea4edd14
Ardie Saeidi <ardalan.saeidi@gmail.com>
2012-01-19 11:26:50 -0800

    Initial commit

---
+++ example/people.js
@@ -0,0 +1,21 @@
+var Hasshu = require('../hasshu');
+
+
+var people = new Hasshu({'Joe': { age: 25 }, 'Jane': { age: 23 } });
+
+
+// enumerate over hash
+people.forEach(function(key, val) {
+	console.log('value for key: %s is ', key, val);
+});
+
+// adding a new person
+people.set('John', { age: 32 });
+console.log('Just added John: ', people.get('John'));
+
+// getting all the keys
+console.log('Keys in hash: ', people.keys());
+
+// getting all the values 
+console.log('Values in hash: ', people.values());
+
---
+++ hasshu.js
@@ -0,0 +1,94 @@
+module.exports = Hasshu;
+
+
+/*
+ * Hasshu (hash in japanese) is a simple hash library.
+ * @param hash: Pass in hash object. (Optional)
+ * 	ex: 
+ *		var hash = new Hasshu({ key1: 'some value' });
+ * 		var hash2 = new Hasshu({ 'key-with-hyphen': 'some other value'}); 
+ */
+function Hasshu(hash) {
+  if (!(this instanceof Hasshu))
+    return new Hasshu(hash);
+
+  if (typeof(hash) === 'undefined')
+		hash = {};
+
+  /*
+   * Will add or update an item in hash.
+   * @param key: Key in hash.
+   * @param val: Value to save.
+   */
+  this.set = function(key, val) {
+    hash[key] = val;
+  };
+
+
+  /*
+   * Gets item in hash by key.
+   * @param key: Key in hash.
+   */
+  this.get = function(key) {
+    return hash[key];
+  };
+
+
+  /*
+   * Remove item from hash by key.
+   * @param key: Key in hash to delete.
+   */
+  this.remove = function(key) {
+    delete hash[key];
+  };
+
+
+  /*
+   * Returns true if key exists in hash.
+   * @param key: Key to check.
+   */
+  this.hasKey = function(key) {
+    return hash.hasOwnProperty(key);
+  };
+
+
+  /*
+   * Iterate through hash.
+   * @param cb: Anon function with (key, val) passed in.
+   */
+  this.forEach = function(cb) {
+		this.keys().forEach(function(key) {
+			cb(key, hash[key]);
+		});
+  };
+
+
+  /*
+   * Returns array of keys in hash.
+   */
+  this.keys = function() {
+    return Object.keys(hash);
+  };
+
+
+	/*
+   * Returns the number of items in hash.
+   */
+	this.length = function() {
+		return this.keys().length;
+	};
+
+
+	/*
+   * Returns an array of values.
+   */
+	this.values = function() {
+		var values = [];
+
+		this.forEach(function(key, val) {
+			values.push(val);
+		});
+
+		return values;
+	};
+}
